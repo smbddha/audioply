ecc39b57e3ad4bbd57bd125ab406e9323fa23ad5
smbddha <samuelwlaing@gmail.com>
2023-03-28 18:59:16 -0400

    ___

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/*<!-- Google tag (gtag.js) -->*/}
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script strategy="lazyOnload" id="ga-script">
+          {`
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+	`}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
